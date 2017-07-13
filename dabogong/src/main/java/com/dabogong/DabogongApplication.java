20ac3fea152f66cadbebc37f263c3233004bc9a0
foxsugar <redalert3@126.com>
2017-07-13 14:29:16 +0800

    first commit

---
+++ dabogong/src/main/java/com/dabogong/DabogongApplication.java
@@ -0,0 +1,12 @@
+package com.dabogong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabogongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabogongApplication.class, args);
+	}
+}
---
+++ dabogong/src/test/java/com/dabogong/DabogongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dabogong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DabogongApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
